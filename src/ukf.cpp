74b58fac988081095cdabc80c27338edf5bbcb3a
Heyjoy <iheyjoy@gmail.com>
2017-08-13 19:54:29 +0800

    20170813

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -78,7 +78,10 @@ UKF::UKF() {
 	  double weight = 0.5 / (n_aug_ + lambda_);
 	  weights_(i) = weight;
   }
-
+  //for the first measurment
+  is_initialized_ = false;
+  // previous timestamp
+  previous_timestamp_=0;
 }
 
 UKF::~UKF() {}
@@ -94,6 +97,49 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+	if (use_laser_ == false && meas_package.sensor_type_ == MeasurementPackage::LASER) return;
+	if (use_radar_ == false && meas_package.sensor_type_ == MeasurementPackage::RADAR) return;
+  /*****************************************************************************
+  *  Initialization
+  ****************************************************************************/
+	if (!is_initialized_) {
+		// first measurement
+		cout << "UKF:initializ " << endl;
+		float  px, py, v,vx, vy, yaw;
+
+		if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+			/**
+			Convert radar from polar to cartesian coordinates and initialize state.
+			*/
+			float rho = meas_package.raw_measurements_[0];
+			float phi = meas_package.raw_measurements_[1];
+			float rho_dot = meas_package.raw_measurements_[2];
+			px = rho * cos(phi);
+			py = rho * sin(phi);
+			vx = rho_dot * cos(phi);
+			vy = rho_dot * sin(phi);
+			v = sqrt(vx*vx + vy*vy);
+			yaw = atan2(vy, vx);
+			x_ << px, py, v, yaw,0;
+		}
+		else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+			/**
+			Initialize state.
+			*/
+			px = meas_package.raw_measurements_(0);
+			py = meas_package.raw_measurements_(1);
+			x_ << px, py, 0, 0, 0;
+		}
+
+		// done initializing, no need to predict or update
+		is_initialized_ = true;
+		previous_timestamp_ = meas_package.timestamp_;
+		return;
+	}
+	float dt = (meas_package.timestamp_ - previous_timestamp_) / 1000000.0;
+	previous_timestamp_ = meas_package.timestamp_;
+
+
 }
 
 /**
@@ -138,4 +184,68 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+}
+void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
+
+	//set state dimension
+	int n_x = 5;
+
+	//define spreading parameter
+	double lambda = 3 - n_x;
+
+	//set example state
+	VectorXd x = VectorXd(n_x);
+	x << 5.7441,
+		1.3800,
+		2.2049,
+		0.5015,
+		0.3528;
+
+	//set example covariance matrix
+	MatrixXd P = MatrixXd(n_x, n_x);
+	P << 0.0043, -0.0013, 0.0030, -0.0022, -0.0020,
+		-0.0013, 0.0077, 0.0011, 0.0071, 0.0060,
+		0.0030, 0.0011, 0.0054, 0.0007, 0.0008,
+		-0.0022, 0.0071, 0.0007, 0.0098, 0.0100,
+		-0.0020, 0.0060, 0.0008, 0.0100, 0.0123;
+
+	//create sigma point matrix
+	MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
+
+	//calculate square root of P
+	MatrixXd A = P.llt().matrixL();
+
+	/*******************************************************************************
+	* Student part begin
+	******************************************************************************/
+
+	//set first column of sigma point matrix
+	Xsig.col(0) = x;
+
+	//set remaining sigma points
+	for (int i = 0; i < n_x; i++)
+	{
+		Xsig.col(i + 1) = x + sqrt(lambda + n_x) * A.col(i);
+		Xsig.col(i + 1 + n_x) = x - sqrt(lambda + n_x) * A.col(i);
+	}
+
+	/*******************************************************************************
+	* Student part end
+	******************************************************************************/
+
+	//print result
+	//std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+	//write result
+	*Xsig_out = Xsig;
+
+	/* expected result:
+	Xsig =
+	5.7441  5.85768   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441
+	1.38  1.34566  1.52806     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38
+	2.2049  2.28414  2.24557  2.29582   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049
+	0.5015  0.44339 0.631886 0.516923 0.595227   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015
+	0.3528 0.299973 0.462123 0.376339  0.48417 0.418721 0.405627 0.243477 0.329261  0.22143 0.286879
+	*/
+
 }
